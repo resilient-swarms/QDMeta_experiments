51c3973b30242bb6df46bb8f3a12c111d1a56349
bossdm <davidmbossens@gmail.com>
2021-06-05 00:59:21 +0100

    add aurora params

--- meta-cmaes/params.hpp
+++ meta-cmaes/params.hpp
@@ -10,6 +10,71 @@
 using namespace sferes;
 using namespace sferes::gen::evo_float;
 
+#if AURORA()
+
+struct BottomParams
+{
+    SFERES_CONST size_t update_period = 50;
+    SFERES_CONST float K = 10.0;// arbitrary value to have a specific "resolution"; we get 200K + solutions with K=60,000. 7K solutions with K=1200
+    struct nov
+    {
+        static double l;// novelty threshold
+        SFERES_CONST double k = 15;
+        SFERES_CONST double eps = 0.1;
+    };
+
+    // TODO: move to a qd::
+    struct pop
+    {
+        // size of a batch
+        SFERES_CONST size_t size = 200;
+        SFERES_CONST size_t nb_gen = 250001;
+        SFERES_CONST size_t dump_period = 200;
+    };
+    //   struct parameters {
+    //     // used to avoid really degenerated experiments with trajectories not doing anything.
+    //     SFERES_CONST float min = 0.1;
+    //     SFERES_CONST float max = 0.9;
+    //   };
+    // our values for each gait parameter can take on any one of these
+    struct sampled
+    {
+        SFERES_ARRAY(float, values, 0.00f, 0.025f, 0.05f, 0.075f, 0.10f, 0.125f, 0.15f, 0.175f,
+                     0.20f, 0.225f, 0.25f, 0.275f, 0.30f, 0.325f, 0.35f,
+                     0.375f, 0.40f, 0.425f, 0.45f, 0.475f, 0.50f, 0.525f,
+                     0.55f, 0.575f, 0.60f, 0.625f, 0.65f, 0.675f, 0.70f,
+                     0.725f, 0.75f, 0.775f, 0.80f, 0.825f, 0.85f, 0.875f,
+                     0.90f, 0.925f, 0.95f, 0.975f, 1.0f);
+
+        SFERES_CONST float mutation_rate = 0.125f; //1 divided by number of genes
+        SFERES_CONST float cross_rate = 0.00f;
+        SFERES_CONST bool ordered = true; //increment or decrement rather than random selection
+    };
+    // struct evo_float
+    // {
+    //     SFERES_CONST float cross_rate = 0.0f;
+    //     SFERES_CONST float mutation_rate = 0.1f;
+    //     SFERES_CONST float eta_m = 10.0f;
+    //     SFERES_CONST float eta_c = 10.0f;
+    //     SFERES_CONST mutation_t mutation_type = polynomial;
+    //     SFERES_CONST cross_over_t cross_over_type = sbx;
+    // };
+    struct qd
+    {
+        SFERES_CONST size_t dim = BEHAV_DIM;
+        SFERES_CONST size_t behav_dim = BEHAV_DIM;
+    };
+        // parameter limits between 0 and 1
+    struct parameters
+    {
+        SFERES_CONST float min = 0.0f;
+        SFERES_CONST float max = 1.0f;
+    };
+
+};
+
+#else
+
 /* params for the bottom-level map */
 struct BottomParams
 {
@@ -77,7 +142,7 @@ struct BottomParams
         SFERES_CONST size_t time = 5;
     };
 };
-
+#endif
 
 #if META()
 /* params for the top-level map */
