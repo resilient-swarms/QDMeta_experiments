d58d140f99a593441eadda17144d0cc1d862599f
David <davidmbossens@gmail.com>
2021-06-05 20:38:49 +0100

    make script

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 cp -arv ~/RHex_experiments/meta-cmaes/ ${BOTS_DIR}/include
-mkdir ${SFERES_DIR}/exp/Rhexps
-cp -arv ~/RHex_experiments/Rhexps ${SFERES_DIR}/exp/Rhexps  # copy wscript to correct folder
+cp -arv ~/RHex_experiments/rhexps ${SFERES_DIR}/exp/  # copy wscript to correct folder
 cd $SFERES_DIR
 ./waf distclean
-./waf configure --exp Rhexps
-./waf --exp Rhexps
+./waf configure --exp rhexps
+./waf --exp rhexps
 
 
