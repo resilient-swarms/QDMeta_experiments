197d1126c38c32ae89ff8849dcbbc2bb8f411e87
bossdm <davidmbossens@gmail.com>
2021-07-02 00:37:34 +0100

    some comments on usage in test script

--- singularity/run_metacmaes_test.sh
+++ singularity/run_metacmaes_test.sh
@@ -8,9 +8,9 @@
  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib/x86_64-linux-gnu:/lib:/usr/lib:/usr/local/lib:/usr/lib/x86_64-linux-gnu:$BOTS_DIR/lib
 
 
- test_type=$1       #  test or train (train not implemented (yet))
-condition_prefix=$2  # damage_meta, envir_meta, damage_control, envir_control
-control_type=$3   # need to supply argument with the method, e.g. b1p1 for meta, "" for random, and "pol" for control
+ test_type=$1       #  test for meta; test_envir or test_damage for control conditions
+condition_prefix=$2  # damage_meta, envir_meta, cvt50D
+control_type=$3   # need to supply argument with the method, e.g. b1p1 for meta, tra for cvt50D
 replicate_number=$4
 RESULTS_DIR=$5 # destination folder
 
