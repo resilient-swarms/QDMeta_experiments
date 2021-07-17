b4911a8f6f515724670ee83007c7dc463a4dfde5
bossdm <davidmbossens@gmail.com>
2021-07-17 16:48:14 +0100

    rename to bottomparams

--- rhexps/aurora.cpp
+++ rhexps/aurora.cpp
@@ -177,7 +177,7 @@ int main(int argc, char **argv)
         (params_t::encoder_type != aurora::EncoderType::lstm_ae) || (params_t::use_videos),
         "Use of LSTM AE => need for use_videos");
 
-    typedef aurora::env::Environment<environment, params_t> env_t;
+    typedef aurora::env::Environment<environment, BottomParams> env_t;
     typedef aurora::algo::AlgorithmFactory<algorithm, env_t>::algo_t algo_t;
 
     algo_t::update_parameters();
