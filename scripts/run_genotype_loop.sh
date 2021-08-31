489e3ab976f32909b774617af86f8042b4e5551f
bossdm <davidmbossens@gmail.com>
2021-08-31 22:15:53 +0100

    video

--- scripts/run_genotype_loop.sh
+++ scripts/run_genotype_loop.sh
@@ -3,5 +3,9 @@
 for i in `seq 0 9`; do 
 	echo $i
 	$SFERES_DIR/build/exp/rhexps/rhex_metaCMAES_testgenotype_binary $1 $i
+
+	ffmpeg -r 24 -i ~/ToyData/video_world${i}_damage_none%4d.png -vcodec libx264 -y -an ~/ToyData/${2}_world${i}.mp4 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"
+	rm ~/ToyData/video_world${i}_damage_none*
 done
+
 
